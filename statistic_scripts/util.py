2a51122b080a6ee63e93dbceee2d20bf2b3fdfc3
Laurens Versluis <lvs215@fs0.cm.cluster>
2021-05-15 21:36:23 +0200

    Start of polishing graphs

--- statistic_scripts/util.py
+++ statistic_scripts/util.py
@@ -12,11 +12,11 @@ def generate_cdf(spark_dataframe, column, output_path, hor_axis_label, vert_axis
             F.col(column) >= 0).withColumnRenamed(column, "target")
         if df.count() > 1000:  # DF is too large, use sampling to get the distribution.
             # Get the 1000 permilles
-            permilles =  df.approxQuantile("target", [float(i) / 1000 for i in range(0, 1001)], 0.001)
+            permilles =  df.approxQuantile("target", range(0, 1.001, 0.001), 0.001)
             df = pd.DataFrame({"target": permilles})
             return df
         else:
-            return df.toPandas()
+            return df.toPandas().dropna()
 
     def normalize_df(df):
         df.sort_values(df.columns[0], inplace=True)
@@ -28,15 +28,18 @@ def generate_cdf(spark_dataframe, column, output_path, hor_axis_label, vert_axis
             df["cdf"] = df["pdf"].cumsum()
 
     pdf = get_column(spark_dataframe)
+    
+    offset_text = ""
 
     if len(pdf) == 0:  # If the dataframe after filtering is empty, then print this metric is not available
         import matplotlib.pyplot as plt
         fig = plt.figure(figsize=(6, 3))
         ax = fig.add_subplot(111)
-        ax.set_xlabel(hor_axis_label)
-        ax.set_ylabel(vert_axis_label)
+        ax.set_xlabel(hor_axis_label.format(offset_text), fontsize=18)
+        ax.set_ylabel(vert_axis_label, fontsize=18)
         ax.text(0.5, 0.5, 'This trace does not contain this information.', horizontalalignment='center',
-                verticalalignment='center', transform=ax.transAxes, fontsize=10)
+                verticalalignment='center', transform=ax.transAxes, fontsize=12)
+
         fig.savefig(output_path, dpi=600, format="png")
         return
 
@@ -47,7 +50,7 @@ def generate_cdf(spark_dataframe, column, output_path, hor_axis_label, vert_axis
         normalize_df(gp)
         return gp
 
-    count_df = normalize_group(pdf).reset_index().dropna()
+    count_df = normalize_group(pdf).reset_index()
 
     # Add a row at the start so that the CDF starts at 0 and ends at 1 (in case we only have one datapoint in the DF)
     count_df.index = count_df.index + 1  # shifting index
@@ -56,39 +59,28 @@ def generate_cdf(spark_dataframe, column, output_path, hor_axis_label, vert_axis
     count_df.loc[len(count_df)] = [len(count_df), math.inf, 1, 1]
     count_df.sort_index(inplace=True)
 
-    def formatOneLabel(x):
-        if x == 0:
-            return "0"
-        else:
-            exponentNum = str(np.int32(np.floor(np.log10(x))))
-            return "$10^{" + exponentNum + "}$"
-
-    def formatYaxisLabels(xl):
-        return list(map(formatOneLabel, xl))
-
-    # plt = ggplot(count_df) + \
-    #       theme_light(base_size=18) + \
-    #       geom_point(aes(x="target", y="cdf", ), stat=stat_identity, size=3,
-    #                  fill="none") + \
-    #       geom_line(aes(x="target", y="cdf", ), stat=stat_identity) + \
-    #       xlab(hor_axis_label) + \
-    #       ylab(vert_axis_label) + \
-    #       ylim(0, 1) + \
-    #       scale_x_log10(labels=formatYaxisLabels)
-
     plt = ggplot(count_df) + \
             theme_light(base_size=18) + \
-            geom_step(aes(x="target", y="cdf"), size=1) +\
-            xlab(hor_axis_label) + \
-            ylab(vert_axis_label)
-            # scale_x_continuous(labels=formatYaxisLabels)
+            geom_step(aes(x="target", y="cdf"), size=1)
+        
+    # Make more readible the labels
+    fig = plt.gcf()
+    ax = plt.gca()
+    ax.get_xaxis().get_offset_text().set_visible(False)
+    ax.get_xaxis().set_major_formatter(ScalarFormatter(useMathText=True))
+    fig.tight_layout()  # Need to set this to be able to get the offset... for whatever reason
+    offset_text = ax.get_xaxis().get_major_formatter().get_offset()
+    
+    ax.set_xlabel("koek", fontsize=18)
+    ax.set_ylabel(vert_axis_label, fontsize=18)
 
-    ggsave(filename=output_path,
-           plot=plt,
-           device='png',
-           dpi=600,
-           height=3,
-           width=6)
+    fig.savefig(output_path, device='png', dpi='600', figsize=(6,3))
+#     ggsave(filename=output_path,
+#            plot=fig,
+#            device='png',
+#            dpi=600,
+#            height=3,
+#            width=6)
 
     if show:
         print(plt)
