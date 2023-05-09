1cd1413392915eb5bd243606a409a7a935fcbe39
Philip Cho <p.k.cho@student.tudelft.nl>
2023-05-09 23:35:12 +0200

    create benchmark for sparkmeasure

---
+++ benchmarking/src/main/java/com/asml/apa/wta/BenchmarkRunner.java
@@ -0,0 +1,19 @@
+package com.asml.apa.wta;
+
+import org.openjdk.jmh.profile.GCProfiler;
+import org.openjdk.jmh.profile.StackProfiler;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class BenchmarkRunner {
+
+    public static void main(String[] args) throws Exception {
+        Options options = new OptionsBuilder()
+                .include(SparkSQLBenchmark.class.getSimpleName())
+                .addProfiler(StackProfiler.class)
+                .addProfiler(GCProfiler.class)
+                .build();
+        new Runner(options).run();
+    }
+}
