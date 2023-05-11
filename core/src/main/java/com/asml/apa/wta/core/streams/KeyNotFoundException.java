944ec665ce276767b025cf038e8155aa48968348
amousavigourab <a.mousavigourabi@student.tudelft.nl>
2023-05-11 18:36:16 +0200

    Adds some documentation to KeyNotFoundException

--- core/src/main/java/com/asml/apa/wta/core/streams/KeyNotFoundException.java
+++ core/src/main/java/com/asml/apa/wta/core/streams/KeyNotFoundException.java
@@ -11,7 +11,14 @@ public class KeyNotFoundException extends RuntimeException {
 
   private static final long serialVersionUID = -3875011289743716105L;
 
+  /**
+   * Constructs a {@link KeyNotFoundException} with the key in the message.
+   *
+   * @param key the key the user tried to use
+   * @author Atour Mousavi Gourabi
+   * @since 1.0.0
+   */
   public KeyNotFoundException(Object key) {
-    super("Specified key " + key.toString() + " does not exist in the KeyedStream");
+    super("Specified key " + key + " does not exist in the KeyedStream");
   }
 }
