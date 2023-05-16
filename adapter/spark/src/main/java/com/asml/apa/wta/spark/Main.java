80c694c9b9a323f57763b3ed178cf161cb9c5dcc
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2023-05-16 13:06:14 +0200

    create logging test

--- adapter/spark/src/main/java/com/asml/apa/wta/spark/Main.java
+++ adapter/spark/src/main/java/com/asml/apa/wta/spark/Main.java
@@ -1,7 +1,12 @@
 package com.asml.apa.wta.spark;
 
+import org.apache.log4j.Logger;
+
 public class Main {
 
   public static void main(String[] args) {
+    Logger logger = PluginLogger.getInstance();
+    PluginLogger.loadConfig();
+    System.out.println(logger.getAllAppenders().hasMoreElements());
   }
 }
--- adapter/spark/src/test/java/com/asml/apa/wta/spark/LoggingTest.java
+++ adapter/spark/src/test/java/com/asml/apa/wta/spark/LoggingTest.java
@@ -1,22 +1,12 @@
 package com.asml.apa.wta.spark;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.apache.log4j.*;
+import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
-public class LoggingTest {
+class LoggingTest {
 
     private Logger logger;
-
-    private static void testConfigLoad() throws IOException {
-        Properties props = new Properties();
-        props.load(new FileInputStream("src/test/resources/log4j2.properties"));
-        PropertyConfigurator.configure(props);
-    }
 
     @Test
     public void loggerNotNullAfterInit() {
@@ -27,12 +17,7 @@ public class LoggingTest {
 
     @Test
     public void loggerSameInstance() {
-        assertThat(PluginLogger.getInstance()).isEqualTo(PluginLogger.getInstance());
-    }
-
-    @Test
-    public void parsedSuccessful() throws IOException {
         logger = PluginLogger.getInstance();
-        testConfigLoad();
+        assertThat(logger).isEqualTo(PluginLogger.getInstance());
     }
 }
