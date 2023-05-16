fd754a52ff33d21fafa500321694a7735f963bd4
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2023-05-16 10:53:07 +0200

    add logging infrastructure

--- adapter/spark/src/main/java/com/asml/apa/wta/spark/Main.java
+++ adapter/spark/src/main/java/com/asml/apa/wta/spark/Main.java
@@ -1,7 +1,18 @@
 package com.asml.apa.wta.spark;
 
+import org.apache.log4j.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
-  public static void main(String[] args) {
-    // System.out.println("Hello World!");
+
+  public static Logger logger = LogManager.getLogger(Main.class);
+  public static void main(String[] args) throws IOException {
+    Properties props = new Properties();
+    props.load(new FileInputStream("adapter/spark/log4j2.properties"));
+    PropertyConfigurator.configure(props);
+    logger.info("Hello World");
   }
 }
