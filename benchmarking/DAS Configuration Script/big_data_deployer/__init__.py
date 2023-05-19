0668bcc01f543cafff90b7312b6c8771739a3c4f
Tianchen Qu <T.QU@student.tudelft.nl>
2023-05-19 21:53:28 +0200

    remove unnecessary part

--- benchmarking/DAS Configuration Script/big_data_deployer/__init__.py
+++ benchmarking/DAS Configuration Script/big_data_deployer/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
 
 from frameworkmanager import Framework, FrameworkVersion, FrameworkRegistry, FrameworkManager, get_framework_registry
-import hadoop, zookeeper, spark, influxdb
+import hadoop, zookeeper, spark
 
