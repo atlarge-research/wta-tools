3d1f91338fc1aef710951898ab7ab7935d1da50c
amousavigourab <a.mousavigourabi@student.tudelft.nl>
2023-05-20 15:18:14 +0200

    Keeps the linters happy

--- core/src/main/java/com/asml/apa/wta/core/logger/Log4j2Configuration.java
+++ core/src/main/java/com/asml/apa/wta/core/logger/Log4j2Configuration.java
@@ -5,8 +5,32 @@ import com.asml.apa.wta.core.utils.WtaUtils;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.config.Configurator;
 
+/**
+ * Logging utils to set up part of the Log4j2 configuration.
+ *
+ * @author Atour Mousavi Gourabi
+ * @since 1.0.0
+ */
 public class Log4j2Configuration {
 
+  /**
+   * Private constructor that throws an exception to ensure the utils class cannot be instantiated.
+   *
+   * @throws IllegalStateException when invoked
+   * @author Atour Mousavi Gouraib
+   * @since 1.0.0
+   */
+  private Log4j2Configuration() {
+    throw new IllegalStateException();
+  }
+
+  /**
+   * Sets up the parts of the logging configuration that are set by user input.
+   * Sets the logging level in accordance with the user input in config.json.
+   *
+   * @author Atour Mousavi Gourabi
+   * @since 1.0.0
+   */
   public static void setUpLoggingConfig() {
     RuntimeConfig config = WtaUtils.readConfig();
     Level logLevel;
