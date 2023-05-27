e347f021b4f540b6f775395d1efb03342ced29ab
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2023-05-27 12:04:30 +0200

    recover runner class

---
+++ benchmarking/spark-benchmark/src/main/java/com/asml/apa/wta/benchmarking/spark/BenchmarkRunner.java
@@ -0,0 +1,16 @@
+package com.asml.apa.wta.benchmarking.spark;
+
+/**
+ * Entry point for running the Spark benchmark.
+ * @author Pil Kyu CHo
+ * @since 1.0.0
+ */
+public class BenchmarkRunner {
+
+  /**
+   * Private constructor to avoid being initialized.
+   */
+  private BenchmarkRunner() {}
+
+  public static void main(String[] args) {}
+}
