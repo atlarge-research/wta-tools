edb5aca00822014943c648e3011c1a4ec7a70df3
amousavigourab <a.mousavigourabi@student.tudelft.nl>
2023-05-30 13:19:02 +0200

    Make GSON serialize the domain correctly

--- core/src/main/java/com/asml/apa/wta/core/model/enums/Domain.java
+++ core/src/main/java/com/asml/apa/wta/core/model/enums/Domain.java
@@ -1,6 +1,6 @@
 package com.asml.apa.wta.core.model.enums;
 
-import lombok.AllArgsConstructor;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Domain enum for WTA traces.
@@ -9,11 +9,18 @@ import lombok.AllArgsConstructor;
  * @author Henry Page
  * @since 1.0.0
  */
-@AllArgsConstructor
 public enum Domain {
+
+  @SerializedName("Biomedical")
   BIOMEDICAL,
+
+  @SerializedName("Engineering")
   ENGINEERING,
+
+  @SerializedName("Industrial")
   INDUSTRIAL,
+
+  @SerializedName("Scientific")
   SCIENTIFIC;
 
   public static final String[] STRINGS = {"Biomedical", "Engineering", "Industrial", "Scientific"};
