8742f7b698e512cb817e154b4ed619bcedfae034
Tianchen Qu <T.QU@student.tudelft.nl>
2023-05-30 22:21:51 +0200

    doc

--- core/src/main/java/com/asml/apa/wta/core/model/BaseTraceObject.java
+++ core/src/main/java/com/asml/apa/wta/core/model/BaseTraceObject.java
@@ -23,5 +23,12 @@ public interface BaseTraceObject extends Serializable {
     return "1.0";
   }
 
+  /**
+   * All WTA objects that is stored in the parquet file need this method to convert the object to record for the writer.
+   *
+   * @param checker array indicating what column to ignore
+   * @param schema schema for the output object
+   * @return record of the object
+   */
   GenericRecord convertToRecord(Boolean[] checker, Schema schema);
 }
