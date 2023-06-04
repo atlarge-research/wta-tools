61365861963403166130490ef778deff0f7bd2a9
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2023-06-04 11:54:01 +0200

    checkstyle fix

--- core/src/main/java/com/asml/apa/wta/core/datasource/PerfDataSource.java
+++ core/src/main/java/com/asml/apa/wta/core/datasource/PerfDataSource.java
@@ -53,7 +53,7 @@ public class PerfDataSource {
    */
   public double gatherMetrics() throws ExecutionException, InterruptedException {
     CompletableFuture<String> energyMetrics = bashUtils.executeCommand(
-          "perf stat -e power/energy-pkg/ -a sleep 1 2>&1 | grep -m 1 -oP \"^\\s+\\K[\\d,]+(?=\\s+Joules)\"");
+        "perf stat -e power/energy-pkg/ -a sleep 1 2>&1 | grep -m 1 -oP \"^\\s+\\K[\\d,]+(?=\\s+Joules)\"");
     return Double.parseDouble(energyMetrics.get());
   }
 }
