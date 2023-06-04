4ad9cacc71ed7a2ba352165c4b9ad1e7f2b7c377
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2023-06-04 17:32:53 +0200

    test only valid for linux system

--- core/src/test/java/com/asml/apa/wta/core/datasource/PerfDataSourceIntegrationTest.java
+++ core/src/test/java/com/asml/apa/wta/core/datasource/PerfDataSourceIntegrationTest.java
@@ -1,9 +1,11 @@
 package com.asml.apa.wta.core.datasource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.condition.OS.LINUX;
 
 import com.asml.apa.wta.core.utils.BashUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
 
 public class PerfDataSourceIntegrationTest {
 
@@ -11,12 +13,14 @@ public class PerfDataSourceIntegrationTest {
 
   private final PerfDataSource sut = new PerfDataSource(bashUtils);
 
-  @Test
+  @Test()
+  @EnabledOnOs(LINUX)
   void perfEnergyDataSourceIsAvailable() {
     assertThat(sut.isAvailable()).isTrue();
   }
 
   @Test
+  @EnabledOnOs(LINUX)
   void perfEnergyGatherMetricsSuccessful() {
     assertThat(sut.gatherMetrics()).isGreaterThan(0.0);
   }
