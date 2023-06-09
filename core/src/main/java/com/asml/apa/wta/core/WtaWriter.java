fd0c5ea95d233ee988ee5f68dc9bbac766657d8e
amousavigourab <a.mousavigourabi@student.tudelft.nl>
2023-06-10 00:45:06 +0200

    Add Javadoc for write methods

--- core/src/main/java/com/asml/apa/wta/core/WtaWriter.java
+++ core/src/main/java/com/asml/apa/wta/core/WtaWriter.java
@@ -41,30 +41,29 @@ public class WtaWriter {
     setupDirectories(path, version);
   }
 
-  public void write(ResourceStore resources) {
-    ParquetSchema schema = new ParquetSchema(Resource.class, resources.getResources(), "resources");
-    OutputFile path = file.resolve("resources").resolve(schemaVersion).resolve("resource.parquet");
-    try (ParquetWriter<Resource> resourceWriter = new ParquetWriter<>(path, schema)) {
-      for (Resource resource : resources.getResources()) {
-        resourceWriter.write(resource);
-      }
+  /**
+   * Writes a {@link Workload} to the corresponding JSON file.
+   *
+   * @param workload the workload to write
+   * @author Atour Mousavi Gourabi
+   * @since 1.0.0
+   */
+  public void write(Workload workload) {
+    OutputFile path = file.resolve("workload").resolve(schemaVersion).resolve("generic_information.json");
+    try (JsonWriter<Workload> workloadWriter = new JsonWriter<>(path)) {
+      workloadWriter.write(workload);
     } catch (IOException e) {
-      log.error("Could not write resources to file.");
+      log.error("Could not write workload to file.");
     }
   }
 
-  public void write(TaskStore tasks) {
-    ParquetSchema schema = new ParquetSchema(Task.class, tasks.getTasks(), "tasks");
-    OutputFile path = file.resolve("tasks").resolve(schemaVersion).resolve("task.parquet");
-    try (ParquetWriter<Task> taskWriter = new ParquetWriter<>(path, schema)) {
-      for (Task task : tasks.getTasks()) {
-        taskWriter.write(task);
-      }
-    } catch (IOException e) {
-      log.error("Could not write tasks to file.");
-    }
-  }
-
+  /**
+   * Writes a {@link WorkflowStore} of {@link Workflow}s to the corresponding Parquet file.
+   *
+   * @param workflows the workflows to write
+   * @author Atour Mousavi Gourabi
+   * @since 1.0.0
+   */
   public void write(WorkflowStore workflows) {
     ParquetSchema schema = new ParquetSchema(Workflow.class, workflows.getWorkflows(), "workflows");
     OutputFile path = file.resolve("workflows").resolve(schemaVersion).resolve("workflow.parquet");
@@ -77,12 +76,41 @@ public class WtaWriter {
     }
   }
 
-  public void write(Workload workload) {
-    OutputFile path = file.resolve("workload").resolve(schemaVersion).resolve("generic_information.json");
-    try (JsonWriter<Workload> workloadWriter = new JsonWriter<>(path)) {
-      workloadWriter.write(workload);
+  /**
+   * Writes a {@link TaskStore} of {@link Task}s to the corresponding Parquet file.
+   *
+   * @param tasks the tasks to write
+   * @author Atour Mousavi Gourabi
+   * @since 1.0.0
+   */
+  public void write(TaskStore tasks) {
+    ParquetSchema schema = new ParquetSchema(Task.class, tasks.getTasks(), "tasks");
+    OutputFile path = file.resolve("tasks").resolve(schemaVersion).resolve("task.parquet");
+    try (ParquetWriter<Task> taskWriter = new ParquetWriter<>(path, schema)) {
+      for (Task task : tasks.getTasks()) {
+        taskWriter.write(task);
+      }
     } catch (IOException e) {
-      log.error("Could not write workload to file.");
+      log.error("Could not write tasks to file.");
+    }
+  }
+
+  /**
+   * Writes a {@link ResourceStore} of {@link Resource}s to the corresponding Parquet file.
+   *
+   * @param resources the resources to write
+   * @author Atour Mousavi Gourabi
+   * @since 1.0.0
+   */
+  public void write(ResourceStore resources) {
+    ParquetSchema schema = new ParquetSchema(Resource.class, resources.getResources(), "resources");
+    OutputFile path = file.resolve("resources").resolve(schemaVersion).resolve("resource.parquet");
+    try (ParquetWriter<Resource> resourceWriter = new ParquetWriter<>(path, schema)) {
+      for (Resource resource : resources.getResources()) {
+        resourceWriter.write(resource);
+      }
+    } catch (IOException e) {
+      log.error("Could not write resources to file.");
     }
   }
 
