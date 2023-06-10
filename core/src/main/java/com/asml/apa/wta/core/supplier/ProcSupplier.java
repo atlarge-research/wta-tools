5c2dd398683602864a1d0fb461e53c6c97c925b9
Lohithsai Yadala Chanchu <l.s.yadalachanchu@student.tudelft.nl>
2023-06-10 14:38:03 +0200

    Update ProcSupplier.java

--- core/src/main/java/com/asml/apa/wta/core/supplier/ProcSupplier.java
+++ core/src/main/java/com/asml/apa/wta/core/supplier/ProcSupplier.java
@@ -56,10 +56,7 @@ public class ProcSupplier implements InformationSupplier<ProcDto> {
    * @since 1.0.0
    */
   public CompletableFuture<ProcDto> getSnapshot() {
-    if (isProcAvailable) {
-      log.info("running snapshot");
-      log.info(SystemUtils.OS_NAME);
-      CompletableFuture<Optional<Long>[]> diskStatsFuture = getDiskMetrics();
+    if (isProcAvailable) { CompletableFuture<Optional<Long>[]> diskStatsFuture = getDiskMetrics();
       CompletableFuture<Optional<Long>[]> memStatsFuture = getMemMetrics();
       CompletableFuture<Optional<String>> cpuModelFuture = getCpuModel();
       CompletableFuture<Optional<Double>[]> loadAvgStatsFuture = getLoadAvgMetrics();
