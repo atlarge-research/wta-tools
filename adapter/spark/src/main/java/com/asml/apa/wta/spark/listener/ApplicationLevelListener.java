8ff11da069c1eeeafa39bd309c192560120d906e
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2023-06-12 23:16:17 +0200

    apply linters

--- adapter/spark/src/main/java/com/asml/apa/wta/spark/listener/ApplicationLevelListener.java
+++ adapter/spark/src/main/java/com/asml/apa/wta/spark/listener/ApplicationLevelListener.java
@@ -65,9 +65,7 @@ public class ApplicationLevelListener extends AbstractListener<Workload> {
    * @param applicationEnd The event corresponding to the end of the application
    */
   public void onApplicationEnd(SparkListenerApplicationEnd applicationEnd) {
-
-    // we should enver enter this branch, this is a guard since an application
-    // only terminates once.
+    // we should never enter this branch, this is a guard since an application only terminates once.
     List<Workload> processedObjects = this.getProcessedObjects();
     if (!processedObjects.isEmpty()) {
       log.error("Application end called twice, this should never happen");
@@ -93,7 +91,7 @@ public class ApplicationLevelListener extends AbstractListener<Workload> {
                 .getStageToTasks()
                 .getOrDefault(x, new ArrayList<>())
                 .toArray(new Long[0])))
-            .toArray(size -> new Long[size]);
+            .toArray(Long[]::new);
         task.setParents(ArrayUtils.toPrimitive(parents));
       }
 
