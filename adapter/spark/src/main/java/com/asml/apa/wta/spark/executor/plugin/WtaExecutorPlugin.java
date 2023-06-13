08c9dfaa6249898cab16e93ddc7823e0303e170d
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2023-06-13 13:33:55 +0200

    fix mistake

--- adapter/spark/src/main/java/com/asml/apa/wta/spark/executor/plugin/WtaExecutorPlugin.java
+++ adapter/spark/src/main/java/com/asml/apa/wta/spark/executor/plugin/WtaExecutorPlugin.java
@@ -113,10 +113,10 @@ public class WtaExecutorPlugin implements ExecutorPlugin {
   public void shutdown() {
     if (error) {
       log.error("Shutting down WTA executor plugin due to driver failure.");
-      supplierEngine.stopPinging();
-      supplierEngine.stopSynchronizing();
       return;
     }
+    supplierEngine.stopPinging();
+    supplierEngine.stopSynchronizing();
     log.info("Shutting down WTA executor with no error.");
   }
 }
