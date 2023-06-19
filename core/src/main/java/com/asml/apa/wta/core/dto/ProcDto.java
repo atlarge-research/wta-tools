1b6d27bb9391573e1a61bdeea57fd33459639495
amousavigourab <a.mousavigourabi@student.tudelft.nl>
2023-06-20 01:54:36 +0200

    Empty String as default value over null
    See !97

--- core/src/main/java/com/asml/apa/wta/core/dto/ProcDto.java
+++ core/src/main/java/com/asml/apa/wta/core/dto/ProcDto.java
@@ -223,7 +223,7 @@ public class ProcDto implements SupplierDto {
   private long directMap1G = -1;
 
   @Builder.Default
-  private String cpuModel = null;
+  private String cpuModel = "";
 
   @Builder.Default
   private double loadAvgOneMinute = -1.0;
