2a0023a89da30a203f25411c41eb30f0f014fd1c
amousavigourab <a.mousavigourabi@student.tudelft.nl>
2023-06-25 00:46:58 +0200

    Updates Parquet OutputFile
    As discussed with TA

--- core/src/main/java/com/asml/apa/wta/core/io/DiskParquetOutputFile.java
+++ core/src/main/java/com/asml/apa/wta/core/io/DiskParquetOutputFile.java
@@ -4,6 +4,7 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.parquet.io.OutputFile;
 import org.apache.parquet.io.PositionOutputStream;
@@ -17,15 +18,104 @@ import org.apache.parquet.io.PositionOutputStream;
 @Slf4j
 public class DiskParquetOutputFile implements OutputFile {
 
+  private class LocalPositionOutputStream extends PositionOutputStream {
+
+    private final BufferedOutputStream stream;
+    private long pos = 0;
+
+    public LocalPositionOutputStream(int buffer, StandardOpenOption... openOption) throws IOException {
+      stream = new BufferedOutputStream(Files.newOutputStream(path, openOption), buffer);
+    }
+
+    /**
+     * Get current position in the {@link BufferedOutputStream}.
+     *
+     * @return the current position
+     * @author Atour Mousavi Gourabi
+     * @since 1.0.0
+     */
+    @Override
+    public long getPos() {
+      return pos;
+    }
+
+    /**
+     * Writes a byte from {@code data}.
+     *
+     * @see BufferedOutputStream#write(int)
+     *
+     * @param data the {@code byte}
+     * @throws IOException when something goes wrong during I/O
+     * @author Atour Mousavi Gourabi
+     * @since 1.0.0
+     */
+    @Override
+    public void write(int data) throws IOException {
+      pos++;
+      stream.write(data);
+    }
+
+    /**
+     * Writes the bytes from {@code data}.
+     *
+     * @see BufferedOutputStream#write(byte[])
+     *
+     * @param data the data
+     * @throws IOException when something goes wrong during I/O
+     * @author Atour Mousavi Gourabi
+     * @since 1.0.0
+     */
+    @Override
+    public void write(byte[] data) throws IOException {
+      pos += data.length;
+      stream.write(data);
+    }
+
+    /**
+     * Writes {@code len} bytes from {@code data} starting at {@code off}.
+     *
+     * @see BufferedOutputStream#write(byte[], int, int)
+     *
+     * @param data the data
+     * @param off the start offset in the data
+     * @param len the number of bytes to write
+     * @throws IOException when something goes wrong during I/O
+     * @author Atour Mousavi Gourabi
+     * @since 1.0.0
+     */
+    @Override
+    public void write(byte[] data, int off, int len) throws IOException {
+      pos += len;
+      stream.write(data, off, len);
+    }
+
+    /**
+     * Flushes the {@link DiskParquetOutputFile}.
+     *
+     * @throws IOException when something goes wrong during I/O
+     * @author Atour Mousavi Gourabi
+     * @since 1.0.0
+     */
+    @Override
+    public void flush() throws IOException {
+      stream.flush();
+    }
+
+    /**
+     * Closes the {@link DiskParquetOutputFile}.
+     *
+     * @throws IOException when something goes wrong during I/O
+     * @author Atour Mousavi Gourabi
+     * @since 1.0.0
+     */
+    @Override
+    public void close() throws IOException {
+      stream.close();
+    }
+  }
+
   private final Path path;
 
-  /**
-   * Constructs a {@link DiskParquetOutputFile} from a {@link Path}.
-   *
-   * @param file the path to wrap for the writer
-   * @author Atour Mousavi Gourabi
-   * @since 1.0.0
-   */
   public DiskParquetOutputFile(Path file) {
     path = file;
   }
@@ -41,110 +131,12 @@ public class DiskParquetOutputFile implements OutputFile {
    */
   @Override
   public PositionOutputStream create(long buffer) throws IOException {
-    log.debug("Create org.apache.parquet.io.PositionOutputStream with {} buffer.", buffer);
-
-    return new PositionOutputStream() {
-
-      private final BufferedOutputStream stream =
-          new BufferedOutputStream(Files.newOutputStream(path), (int) buffer);
-      private long pos = 0;
-
-      /**
-       * Get current position in the {@link BufferedOutputStream}.
-       *
-       * @return the current position
-       * @author Atour Mousavi Gourabi
-       * @since 1.0.0
-       */
-      @Override
-      public long getPos() {
-        log.debug("Position at {}.", pos);
-        return pos;
-      }
-
-      /**
-       * Writes a byte from {@code data}.
-       *
-       * @see BufferedOutputStream#write(int)
-       *
-       * @param data the {@code byte}
-       * @throws IOException when something goes wrong during I/O
-       * @author Atour Mousavi Gourabi
-       * @since 1.0.0
-       */
-      @Override
-      public void write(int data) throws IOException {
-        log.debug("Write {}.", data);
-        pos++;
-        stream.write(data);
-      }
-
-      /**
-       * Writes the bytes from {@code data}.
-       *
-       * @see BufferedOutputStream#write(byte[])
-       *
-       * @param data the data
-       * @throws IOException when something goes wrong during I/O
-       * @author Atour Mousavi Gourabi
-       * @since 1.0.0
-       */
-      @Override
-      public void write(byte[] data) throws IOException {
-        log.debug("Write {}.", data);
-        pos += data.length;
-        stream.write(data);
-      }
-
-      /**
-       * Writes {@code len} bytes from {@code data} starting at {@code off}.
-       *
-       * @see BufferedOutputStream#write(byte[], int, int)
-       *
-       * @param data the data
-       * @param off the start offset in the data
-       * @param len the number of bytes to write
-       * @throws IOException when something goes wrong during I/O
-       * @author Atour Mousavi Gourabi
-       * @since 1.0.0
-       */
-      @Override
-      public void write(byte[] data, int off, int len) throws IOException {
-        log.debug("Write {}.", data);
-        pos += len;
-        stream.write(data, off, len);
-      }
-
-      /**
-       * Flushes the {@link DiskParquetOutputFile}.
-       *
-       * @throws IOException when something goes wrong during I/O
-       * @author Atour Mousavi Gourabi
-       * @since 1.0.0
-       */
-      @Override
-      public void flush() throws IOException {
-        stream.flush();
-      }
-
-      /**
-       * Closes the {@link DiskParquetOutputFile}.
-       *
-       * @throws IOException when something goes wrong during I/O
-       * @author Atour Mousavi Gourabi
-       * @since 1.0.0
-       */
-      @Override
-      public void close() throws IOException {
-        stream.close();
-      }
-    };
+    return new LocalPositionOutputStream((int) buffer, StandardOpenOption.CREATE_NEW);
   }
 
   /**
    * Creates a {@link PositionOutputStream} for the wrapped {@link Path}.
-   *
-   * @see DiskParquetOutputFile#create(long)
+   * Overwrites files when they are already present.
    *
    * @param buffer buffer hint
    * @return the created {@link PositionOutputStream}
@@ -154,7 +146,8 @@ public class DiskParquetOutputFile implements OutputFile {
    */
   @Override
   public PositionOutputStream createOrOverwrite(long buffer) throws IOException {
-    return create(buffer);
+    return new LocalPositionOutputStream(
+        (int) buffer, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
   }
 
   /**
@@ -179,5 +172,17 @@ public class DiskParquetOutputFile implements OutputFile {
   @Override
   public long defaultBlockSize() {
     return 512;
+  }
+
+  /**
+   * Returns the path of the {@link com.asml.apa.wta.core.io.OutputFile} as a {@link String}.
+   *
+   * @return the path of this {@link com.asml.apa.wta.core.io.OutputFile} as a {@link String}
+   * @author Atour Mousavi Gourabi
+   * @since 1.0.0
+   */
+  @Override
+  public String getPath() {
+    return path.toString();
   }
 }
