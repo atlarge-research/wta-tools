72cd41fa16f340aa5a80d3d3fe5f71894e83024b
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2023-06-24 19:12:52 +0200

    fix listener integration tests

--- adapter/spark/src/test/java/com/asml/apa/wta/spark/listener/StageLevelListenerIntegrationTest.java
+++ adapter/spark/src/test/java/com/asml/apa/wta/spark/listener/StageLevelListenerIntegrationTest.java
@@ -5,26 +5,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.asml.apa.wta.spark.BaseSparkJobIntegrationTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class StageLevelListenerIntegrationTest extends BaseSparkJobIntegrationTest {
 
   @Test
-  void testGetStageMetricsHasTasksAfterSparkJobAndYieldsNoErrors() {
+  void testGetStageMetricsHasTasksAfterSparkJobAndYieldsNoErrors() throws InterruptedException {
     sut1.registerStageListener();
     invokeJob();
     invokeJob();
-    stopJob();
-    AbstractListener.getThreadPool().shutdown();
+    AbstractListener.getThreadPool().awaitTermination(1, TimeUnit.SECONDS);
     assertThat(sut1.getStageLevelListener().getProcessedObjects().isEmpty()).isFalse();
   }
 
   @Test
-  void runningAJobShouldInitializeWorkflow() {
+  void runningAJobShouldInitializeWorkflow() throws InterruptedException {
     sut1.registerStageListener();
     invokeJob();
     invokeJob();
-    stopJob();
     assertThat(sut1.getStageLevelListener().getStageToJob()).isNotEmpty();
-    AbstractListener.getThreadPool().shutdown();
+    AbstractListener.getThreadPool().awaitTermination(1, TimeUnit.SECONDS);
     assertThat(sut1.getStageLevelListener().getProcessedObjects().isEmpty()).isFalse();
     assertThat(sut1.getStageLevelListener().getProcessedObjects().head().getWorkflowId())
         .isInstanceOf(Long.class)
--- adapter/spark/src/test/java/com/asml/apa/wta/spark/listener/TaskLevelListenerIntegrationTest.java
+++ adapter/spark/src/test/java/com/asml/apa/wta/spark/listener/TaskLevelListenerIntegrationTest.java
@@ -5,26 +5,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.asml.apa.wta.spark.BaseSparkJobIntegrationTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.TimeUnit;
+
 class TaskLevelListenerIntegrationTest extends BaseSparkJobIntegrationTest {
 
   @Test
-  void testGetTaskMetricsHasTasksAfterSparkJobAndYieldsNoErrors() {
+  void testGetTaskMetricsHasTasksAfterSparkJobAndYieldsNoErrors() throws InterruptedException {
     sut1.registerTaskListener();
     invokeJob();
     invokeJob();
-    stopJob();
-    AbstractListener.getThreadPool().shutdown();
+    AbstractListener.getThreadPool().awaitTermination(1, TimeUnit.SECONDS);
     assertThat(sut1.getTaskLevelListener().getProcessedObjects().isEmpty()).isFalse();
   }
 
   @Test
-  void runningAJobShouldInitializeWorkflow() {
+  void runningAJobShouldInitializeWorkflow() throws InterruptedException {
     sut1.registerTaskListener();
     invokeJob();
     invokeJob();
-    stopJob();
     assertThat(sut1.getTaskLevelListener().getStageToJob()).isNotEmpty();
-    AbstractListener.getThreadPool().shutdown();
+    AbstractListener.getThreadPool().awaitTermination(1, TimeUnit.SECONDS);
     assertThat(sut1.getTaskLevelListener().getProcessedObjects().isEmpty()).isFalse();
     assertThat(sut1.getTaskLevelListener().getProcessedObjects().head().getWorkflowId())
         .isInstanceOf(Long.class)
